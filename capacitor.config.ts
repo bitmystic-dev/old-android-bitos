cb79aa918eedd31300551449154ec70ddedce6f1
bitmystic-dev <bitmystic033@gmail.com>
2026-05-22 16:37:13 +0530

    Added Icons

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bitmysticdev.bitos',
+  appName: 'BitOS',
+  webDir: 'dist'
+};
+
+export default config;
